3976d759bed139cca500d39d2d101666c76407bb
Tweedle2Dum <utkarsh.work1405@gmail.com>
2024-03-21 11:32:15 +0530

    add. accordion

--- components/src/App.tsx
+++ components/src/App.tsx
@@ -1,12 +1,30 @@
-import { Button } from "@/Button/Button"
-
+import { Button } from "@/Button/Button";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionContent,
+  AccordionTrigger,
+} from "@/Accordion/Accordion";
 function App() {
-
   return (
     <>
-    <div className="mx-40 my-40"><Button variant={'link'}>Get Started</Button></div>
-     </>
-  )
+      <div className="mx-40 my-40">
+        <Button variant={"link"}>Get Started</Button>
+      </div>
+      <div className="mx-40 my-40">
+        <Accordion type="single">
+          <AccordionItem value="item-1" className=" text-primary">
+            <AccordionTrigger>Hello there</AccordionTrigger>
+            <AccordionContent>This is the content</AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-2" className=" text-primary">
+            <AccordionTrigger>Hello there</AccordionTrigger>
+            <AccordionContent>This is the content</AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
